ccc277fd6466f92e48204455aba5164bfdb578d3
i18n <i18n.site@gmail.com>
2025-02-27 18:03:31 +0800

    🔥

---
+++ user_/src/lib.rs
@@ -0,0 +1,11 @@
+// pub struct User {
+//   pub id: u64,
+// }
+
+// pub async fn name(uid: User) -> Result<String> {
+//   Ok("123".into())
+// }
+//
+// pub async fn name_set(id: User, name: &str) -> Result<()> {
+//   OK
+// }
